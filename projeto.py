170bca1715398232c198b3a2661db53c7c2f57a3
nathalycordsil <nathyc700@gmail.com>
2024-06-14 20:55:14 -0300

    descrição dos códigos

--- projeto.py
+++ projeto.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import ipeadatapy as ip
 import matplotlib.pyplot as plt
 
@@ -8,42 +8,104 @@ st.set_page_config(
     page_icon="👋",
 )
 
-def paginaProjeto():
-    st.title("Lista 4 - Projeto")
-    df = pd.read_csv("https://raw.githubusercontent.com/nathalycordsil/lista4/main/projetos.csv", sep=";") 
-    st.dataframe(df.head(23))
+st.subheader("Exercício Projeto")
+st.title("Exercício Projeto")
 
-    df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
-    df = pd.concat([df, df1])
-    st.write(df.tail())
+code = '''
+df = pd.read_csv("https://raw.githubusercontent.com/nathalycordsil/lista4/main/projetos.csv", sep=";") 
+st.dataframe(df.head(23))
+'''
+st.code(code, language='python')
 
-    colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
-    st.write(df.groupby('ano')[colunas].sum())
+df = pd.read_csv("https://raw.githubusercontent.com/nathalycordsil/lista4/main/projetos.csv", sep=";") 
+st.dataframe(df.head(23))
 
-    fig, ax = plt.subplots()
-    df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
-    st.pyplot(fig)
+st.write("O dataframe foi atualizado adicionando mais uma linha ao final com os dados referentes ao mês de dezembro de 2023.")
+st.write("mes | ano | Projeto1 | Projeto2 | Projeto3 | Projeto4 | Projeto5")
+st.write("--- | --- | -------- | -------- | -------- | -------- | --------")
+st.write("12 | 2023 | 29376 | 40392 | 63648 | 29376 | 25704")
 
-    fig, ax = plt.subplots()
-    df["Projeto1"].plot(kind='hist', ax=ax)
-    df["Projeto4"].plot(kind='hist', ax=ax)
-    st.pyplot(fig)
+st.write("obs: a partir deste ponto, utilize a df atualizada, agora com 24 meses de dados")
 
-def paginaIPEA():
-    st.title("Lista 4 - IPEADATA")
-    st.write(ip.list_series('Selic'))
+code = '''
+df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
+df = pd.concat([df, df1])
+print(df.tail())
+'''
+st.code(code, language='python')
 
-    selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
-    st.write(selic)
+df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
+df = pd.concat([df, df1])
+st.write(df.tail())
 
-    fig, ax = plt.subplots()
-    ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
-    ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
-    st.pyplot(fig)
+st.write("Apresentação da soma dos valores de cada projeto agrupado por ano")
 
-lateral = st.sidebar()
+code = '''
+colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
+st.write(df.groupby('ano')[colunas].sum())
+'''
 
-if lateral.button("Lista 4 - Projeto"):
-    paginaProjeto()
-elif lateral.button("Lista 4 - IPEADATA"):
-    paginaIPEA()
+colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
+st.write(df.groupby('ano')[colunas].sum())
+
+st.write("Gráfico de dispersão cruzando os dados do Projeto1 e Projeto2, com marcadores verdes e em formato de estrela")
+
+code = '''
+fig, ax = plt.subplots()
+df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
+st.pyplot(fig)
+'''
+
+fig, ax = plt.subplots()
+df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
+st.pyplot(fig)
+
+st.write("Gráfico do tipo histograma com os dados do Projeto 1 e Projeto4")
+st.write("Dica: Ao criar dois histograma em sequência, o python agrupa em apenas um desenho")
+
+code = '''
+fig, ax = plt.subplots()
+df["Projeto1"].plot(kind='hist', ax=ax)
+df["Projeto4"].plot(kind='hist', ax=ax)
+st.pyplot(fig)
+'''
+
+fig, ax = plt.subplots()
+df["Projeto1"].plot(kind='hist', ax=ax)
+df["Projeto4"].plot(kind='hist', ax=ax)
+st.pyplot(fig)
+
+st.subheader("Exercício IPEADATA")
+st.title("Exercício IPEADATA")
+
+st.write('Busca na base do IPEADATA os indicadores relacionados a taxa de juros Selic. O objetivo é encontrar o código correspondente ao indicador "Taxa de juros - Over / Selic - acumulada no mês"')
+
+code = '''
+st.write(ip.list_series('Selic'))
+'''
+
+st.write(ip.list_series('Selic'))
+
+st.write('Utilização da biblioteca do IPEADATA para apresentar os valores do indicador "Taxa de juros - Over / Selic - acumulada no mês" dos anos de 2022 e 2023"')
+
+code = '''
+selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+st.write(selic)
+'''
+
+selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+st.write(selic)
+
+st.write("Gráficos de linha, apresentando os meses e valores das taxas, um para o ano de 2022 e outro para o ano de 2023")
+
+code = '''
+fig, ax = plt.subplots()
+ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+st.pyplot(fig)
+'''
+
+fig, ax = plt.subplots()
+ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+st.pyplot(fig)
