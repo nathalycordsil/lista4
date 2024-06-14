4ad799d6645c56855ed9a1b8041fb88c8a6a3c0d
nathalycordsil <nathyc700@gmail.com>
2024-06-14 20:38:14 -0300

    paginas

--- projeto.py
+++ projeto.py
@@ -8,35 +8,42 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.subheader("List 4")
+def paginaProjeto():
+    st.title("Lista 4 - Projeto")
+    df = pd.read_csv("https://raw.githubusercontent.com/nathalycordsil/lista4/main/projetos.csv", sep=";") 
+    st.dataframe(df.head(23))
 
-#EXERCÍCIO PROJETO
-df = pd.read_csv("https://raw.githubusercontent.com/nathalycordsil/lista4/main/projetos.csv", sep=";") 
-st.dataframe(df.head(23))
+    df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
+    df = pd.concat([df, df1])
+    st.write(df.tail())
 
-df1 = pd.DataFrame({'mes': [12], 'ano': [2023], 'Projeto1': [29376], 'Projeto2': [40392], 'Projeto3': [63648], 'Projeto4': [29376], 'Projeto5': [25704] })
-df = pd.concat([df, df1])
-st.write(df.tail())
+    colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
+    st.write(df.groupby('ano')[colunas].sum())
 
-colunas = ['Projeto1', 'Projeto2', 'Projeto3', 'Projeto4', 'Projeto5']
-st.write(df.groupby('ano')[colunas].sum())
+    fig, ax = plt.subplots()
+    df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots()
-df.plot(kind = 'scatter', x = 'Projeto1', y = 'Projeto2', color='darkgreen', marker='*', ax=ax)
-st.pyplot(fig)
+    fig, ax = plt.subplots()
+    df["Projeto1"].plot(kind='hist', ax=ax)
+    df["Projeto4"].plot(kind='hist', ax=ax)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots()
-df["Projeto1"].plot(kind='hist', ax=ax)
-df["Projeto4"].plot(kind='hist', ax=ax)
-st.pyplot(fig)
+def paginaIPEA():
+    st.title("Lista 4 - IPEADATA")
+    st.write(ip.list_series('Selic'))
 
-#EXERCÍCIO IPEADATA
-st.write(ip.list_series('Selic'))
+    selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
+    st.write(selic)
 
-selic = ip.timeseries('BM12_TJOVER12', yearGreaterThan=2021, yearSmallerThan=2024)
-st.write(selic)
+    fig, ax = plt.subplots()
+    ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+    ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
+    st.pyplot(fig)
 
-fig, ax = plt.subplots()
-ip.timeseries('BM12_TJOVER12', year=2021).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
-ip.timeseries('BM12_TJOVER12', year=2022).plot("MONTH", "VALUE ((% a.m.))", ax=ax)
-st.pyplot(fig)
+lateral = st.sidebar()
+
+if lateral.button("Lista 4 - Projeto"):
+    paginaProjeto()
+elif lateral.button("Lista 4 - IPEADATA"):
+    paginaIPEA()
